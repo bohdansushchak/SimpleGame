496e167e7b882f771e62bc60ff422f8fd239b090
AzureAD\BohdanSushchak <bogdansushak@gmail.com>
2017-05-22 22:38:14 +0200

    Stworzono glówne menu gry.

---
+++ Project/Programowanie(gra 2d)/Assets/Scripts/MenuListener.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuListener : MonoBehaviour {
+
+
+    public GameObject PLoadLevel;
+    public GameObject PSettings;
+
+	public void NewGame()
+    {
+        //Application.LoadLevel(1); // для загрузки першого левелу
+    }
+
+    public void Resume()
+    {
+
+    } 
+
+    public void LoadLevel()
+    {
+        if (PSettings.activeSelf)
+            PSettings.SetActive(false);
+
+        PLoadLevel.SetActive(!PLoadLevel.activeSelf);
+    }
+
+    public void Multiplayer()
+    {
+
+    } 
+
+    public void Settings()
+    {
+        if (PLoadLevel.activeSelf)
+            PLoadLevel.SetActive(false);
+
+        PSettings.SetActive(!PSettings.activeSelf);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void Level1()
+    {
+        //Application.LoadLevel(1); // для загрузки першого левелу
+    }
+
+    public void Level2()
+    {
+        //Application.LoadLevel(2); // для загрузки першого левелу
+    }
+
+    public void Level3()
+    {
+        //Application.LoadLevel(3); // для загрузки першого левелу
+    }
+
+    public void Level4()
+    {
+        //Application.LoadLevel(4); // для загрузки першого левелу
+    }
+
+    public void Level5()
+    {
+        //Application.LoadLevel(5); // для загрузки першого левелу
+    }
+
+    public void setMusic(float value)
+    {
+        Settings_value.music = value;
+    }
+
+    public void setSound(float value)
+    {
+        Settings_value.sound = value;
+    }
+}
---
+++ Project/Programowanie(gra 2d)/Assets/Scripts/Settings_value.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Settings_value
+{
+    public static float music;
+    public static float sound;
+
+
+
+
+
+}
