bd638ef9429c95b696cb9d82769f8bc0fc3d1044
DESKTOP-Q8JSAJL\Juli <juli.mach22@gmail.com>
2017-05-28 13:51:26 +0200

    Dodano przejście między poziomami

---
+++ Project/Programowanie(gra 2d)/Assets/Scripts/LoadNextLevel.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadNextLevel : MonoBehaviour {
+
+    public int NextLevel;
+    public float timeWait;
+
+    float time;
+    bool isFinish;
+
+    // Use this for initialization
+    void Start()
+    {
+        isFinish = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (time < Time.time && isFinish)
+            Application.LoadLevel(NextLevel);
+
+    }
+
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == "Player" && !isFinish)
+        {
+            time = Time.time + timeWait;
+            isFinish = true;
+        }
+    }
+}
